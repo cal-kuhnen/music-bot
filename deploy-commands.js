6363b01f0af21722c966508b64db3fa2f449682b
Calvin Kuhnen <calvinkuhnen@gmail.com>
2021-10-19 02:53:33 -0500

    updated commands to register dynamically

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,11 +3,28 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong'),
-  new SlashCommandBuilder().setName('server').setDescription('Replies with server info')
-]
-  .map(command => command.toJSON());
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	import command from `./commands/${file}`;
+	commands.push(command.data.toJSON());
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
